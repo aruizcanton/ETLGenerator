cc47ee7fb99608667fe87c72b10d145e05e9ca1d
aruizcanton <aruizcanton@users.noreply.github.com>
2015-01-26 18:42:20 +0100

    GENERA LOS GRANTS PARA EL USUARIO METADATO
    Debido a una incidencia porque el valor genérico -2 se estaba
    insertando siempre, en lugar del valor -3. Es necesario este proceso
    para dar permisos de SELECT al usuario METADATO para las tablas de
    CATALOGO y así pueda consultar el tipo de los campos.

---
+++ gen_grants_catalogo_para_metadato.sql
@@ -0,0 +1,43 @@
+DECLARE
+  /* CURSOR QUE NOS DARA TODAS LAS TABLAS QUE HAY QUE CREAR */
+  CURSOR dtd_permited_values
+  IS
+    SELECT 
+      ITEM_NAME,
+      ID_LIST,
+      AGREGATION,
+      MAX(LENGTH(VALUE)) LONGITUD
+    FROM METADATO.MTDT_PERMITED_VALUES
+    WHERE ITEM_NAME not in ('TIPO_ENVIO', 'TIPO_SERVICIO')
+    GROUP BY 
+      ITEM_NAME,
+      ID_LIST,
+      AGREGATION
+      ORDER BY ID_LIST;
+  reg_per_val dtd_permited_values%rowtype;
+  num_filas INTEGER; /* ALMACENAREMOS EL NUMERO DE FILAS DE LA TABLA MTDT_PERMITED_VALUES  */
+  longitud_campo INTEGER;
+  clave_foranea INTEGER;  /* 0 Si la tabla no tiene clave foranea. 1 si la tiene  */
+BEGIN
+
+  SELECT COUNT(*) INTO num_filas FROM METADATO.MTDT_PERMITED_VALUES;
+  /* COMPROBAMOS QUE TENEMOS FILAS EN NUESTRA TABLA MTDT_PERMITED_VALUES  */
+  IF num_filas > 0 THEN
+    /* hay filas en la tabla y por lo tanto el proceso tiene cosas que hacer  */
+    DBMS_OUTPUT.put_line('set echo on;');
+    DBMS_OUTPUT.put_line('whenever sqlerror exit 1;');
+    OPEN dtd_permited_values;
+    LOOP
+      /* COMENZAMOS EL BUCLE QUE GENERARA LOS CREATES PARA CADA UNA DE LAS TABLAS */
+      FETCH dtd_permited_values
+      INTO reg_per_val;
+      EXIT WHEN dtd_permited_values%NOTFOUND;
+      --clave_foranea :=0;
+      DBMS_OUTPUT.put_line('GRANT select  on app_mvnodm.DMD_' || reg_per_val.ITEM_NAME || ' to metadato;');
+      DBMS_OUTPUT.put_line('');
+    END LOOP;
+    CLOSE dtd_permited_values;
+    DBMS_OUTPUT.put_line('set echo off;');
+    DBMS_OUTPUT.put_line('exit SUCCESS;');
+  END IF;
+END;
