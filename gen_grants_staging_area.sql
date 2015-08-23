c6a92718d557268d33b63e6c130f92596f0c5d1a
Angel Ruiz Canton <aruizcanton@users.noreply.github.com>
2015-08-23 18:07:34 +0200

    NF: Se añade un grant para el usuario de solo lectura

--- gen_grants_staging_area.sql
+++ gen_grants_staging_area.sql
@@ -10,28 +10,26 @@ DECLARE
       SEPARATOR,
       DELAYED
     FROM MTDT_INTERFACE_SUMMARY;
-  
-  CURSOR dtd_interfaz_detail (concep_name_in IN VARCHAR2, source_in IN VARCHAR2)
+    
+  CURSOR dtd_interfaz_summary_history
   IS
     SELECT 
       CONCEPT_NAME,
       SOURCE,
-      COLUMNA,
-      KEY,
+      INTERFACE_NAME,
       TYPE,
-      LENGTH,
-      NULABLE,
-      POSITION
-    FROM
-      MTDT_INTERFACE_DETAIL
-    WHERE
-      CONCEPT_NAME = concep_name_in and
-      SOURCE = source_in
-      order by POSITION;
+      SEPARATOR,
+      DELAYED,
+      HISTORY
+    FROM MTDT_INTERFACE_SUMMARY
+    where HISTORY is not null;
+    
+  
 
       reg_summary dtd_interfaz_summary%rowtype;
+      reg_summary_history dtd_interfaz_summary_history%rowtype;
+      
 
-      reg_datail dtd_interfaz_detail%rowtype;
       
       primera_col INTEGER;
       TYPE list_columns_primary  IS TABLE OF VARCHAR(30);
@@ -45,6 +43,7 @@ DECLARE
       TABLESPACE_SA                  VARCHAR2(60);
       OWNER_TC                            VARCHAR2(60);
       OWNER_DWH                         VARCHAR2(60);
+      OWNER_RD                            VARCHAR2(60);
       
 
 BEGIN
@@ -56,6 +55,7 @@ BEGIN
   SELECT VALOR INTO TABLESPACE_SA FROM MTDT_VAR_ENTORNO WHERE NOMBRE_VAR = 'TABLESPACE_SA';
   SELECT VALOR INTO OWNER_TC FROM MTDT_VAR_ENTORNO WHERE NOMBRE_VAR = 'OWNER_TC';
   SELECT VALOR INTO OWNER_DWH FROM MTDT_VAR_ENTORNO WHERE NOMBRE_VAR = 'OWNER_DWH';
+  SELECT VALOR INTO OWNER_RD FROM MTDT_VAR_ENTORNO WHERE NOMBRE_VAR = 'OWNER_RD';
   /* (20150119) FIN*/
 
   DBMS_OUTPUT.put_line('set echo on;');
@@ -68,8 +68,27 @@ BEGIN
       DBMS_OUTPUT.put_line('GRANT select, insert, update, delete on ' || OWNER_SA || '.SA_' || reg_summary.CONCEPT_NAME || ' TO ' || OWNER_TC || ';');
       DBMS_OUTPUT.put_line('GRANT select, insert, update, delete on ' || OWNER_SA || '.SA_' || reg_summary.CONCEPT_NAME || ' TO ' || OWNER_DM || ';');
       DBMS_OUTPUT.put_line('GRANT select on ' || OWNER_SA || '.SA_' || reg_summary.CONCEPT_NAME || ' TO ' || OWNER_DWH || ';');
+      
   END LOOP;
   CLOSE dtd_interfaz_summary;
+  
+  /****************************************************************/
+  /* (20150717) ANGEL RUIZ. NUEVA FUNCIONALIDAD.*/
+  /* Las tablas de STAGING pueden tener HISTORICO */
+  /*****************************************************************************/  
+  DBMS_OUTPUT.put_line('');
+ 
+  OPEN dtd_interfaz_summary_history;
+  LOOP
+    FETCH dtd_interfaz_summary_history
+      INTO reg_summary_history;
+      EXIT WHEN dtd_interfaz_summary_history%NOTFOUND;
+      DBMS_OUTPUT.put_line('GRANT select, insert, update, delete on ' || OWNER_SA || '.SAH_' || reg_summary_history.CONCEPT_NAME || ' TO ' || OWNER_TC || ';');
+      DBMS_OUTPUT.put_line('GRANT select, insert, update, delete on ' || OWNER_SA || '.SAH_' || reg_summary_history.CONCEPT_NAME || ' TO ' || OWNER_DM || ';');
+      DBMS_OUTPUT.put_line('GRANT select on ' || OWNER_SA || '.SAH_' || reg_summary_history.CONCEPT_NAME || ' TO ' || OWNER_DWH || ';');
+  END LOOP;
+  CLOSE dtd_interfaz_summary_history;
+  
   DBMS_OUTPUT.put_line('set echo off;');
   DBMS_OUTPUT.put_line('exit SUCCESS;');
 END;
