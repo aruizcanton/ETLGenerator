82cd4bbcc06b41ff1b32411b3ea54e1feea3a006
Angel Ruiz Canton <aruizcanton@users.noreply.github.com>
2015-11-03 15:46:35 +0100

    BUG: SUPRESIÓN CARACTERES EN BLANCO
    Se suprimen caracteres en blanco del nombre de las tablas

--- gen_grants_staging_area.sql
+++ gen_grants_staging_area.sql
@@ -3,7 +3,7 @@ DECLARE
   CURSOR dtd_interfaz_summary
   IS
     SELECT 
-      CONCEPT_NAME,
+      trim(CONCEPT_NAME) "CONCEPT_NAME",
       SOURCE,
       INTERFACE_NAME,
       TYPE,
@@ -14,7 +14,7 @@ DECLARE
   CURSOR dtd_interfaz_summary_history
   IS
     SELECT 
-      CONCEPT_NAME,
+      trim(CONCEPT_NAME) "CONCEPT_NAME",
       SOURCE,
       INTERFACE_NAME,
       TYPE,
