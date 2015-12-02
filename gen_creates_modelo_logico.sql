bb8dda2b6acb0b67358f8cf3ad104faeee2972e1
Angel Ruiz Canton <aruizcanton@users.noreply.github.com>
2015-12-02 18:12:34 +0100

    NADA
    Puro cambio cosmético

--- gen_creates_modelo_logico.sql
+++ gen_creates_modelo_logico.sql
@@ -1,31 +1,4 @@
 DECLARE
-  /* CURSOR QUE NOS DARA TODAS LAS TABLAS QUE HAY QUE CREAR */
-/*  
-  CURSOR c_mtdt_modelo_logico_TABLA
-  IS
-    SELECT 
-      DISTINCT
-      TRIM(TABLE_NAME) "TABLE_NAME",
-      TRIM(TABLESPACE) "TABLESPACE",
-      TRIM(CI) "CI"
-    FROM MTDT_MODELO_LOGICO
-    WHERE CI <> 'P';    /* Las que poseen un valor "P" en esta columna son las tablas de PERMITED_VALUES, por lo que no hya que generar su modelo */
-/*
-  CURSOR c_mtdt_modelo_logico_COLUMNA (table_name_in IN VARCHAR2)
-  IS
-    SELECT 
-      TRIM(TABLE_NAME) "TABLE_NAME",
-      TRIM(COLUMN_NAME) "COLUMN_NAME",
-      DATA_TYPE,
-      PK,
-      TRIM(NULABLE) "NULABLE",
-      CI,
-      TRIM(VDEFAULT) "VDEFAULT",
-      TRIM(TABLESPACE) "TABLESPACE"
-    FROM MTDT_MODELO_LOGICO
-    WHERE
-      TRIM(TABLE_NAME) = table_name_in;
-*/
   /* (20150907) Angel Ruiz . NF: Se crea una tabla de metadato MTDT_MODELO_SUMMARY y otra MTDT_MODELO_DETAIL */
 
     /* CURSOR QUE NOS DARA TODAS LAS TABLAS QUE HAY QUE CREAR */
