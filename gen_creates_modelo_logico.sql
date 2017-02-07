2e4923e0e94af976130b08fb0bfcfee3242e944a
Angel Ruiz Canton <aruizcanton@users.noreply.github.com>
2017-02-07 12:47:50 +0100

    BUG RESUELTO
    Faltaba un trim

--- gen_creates_modelo_logico.sql
+++ gen_creates_modelo_logico.sql
@@ -24,7 +24,7 @@ DECLARE
       TRIM(INDICE) "INDICE"
     FROM MTDT_MODELO_DETAIL
     WHERE
-      TABLE_NAME = table_name_in;
+      trim(TABLE_NAME) = table_name_in;
   /* (20150907) Angel Ruiz . FIN NF: Se crea una tabla de metadato MTDT_MODELO_SUMMARY y otra MTDT_MODELO_DETAIL */
 
   r_mtdt_modelo_logico_TABLA                                          c_mtdt_modelo_logico_TABLA%rowtype;
